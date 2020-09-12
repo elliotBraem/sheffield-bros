e8d48fec4ab62e34022a5e52fda01334b63f7593
Elliot Braem <elliot@brae.me>
2020-09-12 00:54:04 -0500

    Updates

---
+++ precache-manifest.8ed83c41b662cb9e1191a26961888768.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38049431abf92b31aae71dfda610e4a2",
+    "url": "/sheffield-bros/index.html"
+  },
+  {
+    "revision": "8e8f248c301334960525",
+    "url": "/sheffield-bros/static/css/main.dfb0a673.chunk.css"
+  },
+  {
+    "revision": "951e5965a332fdde75d6",
+    "url": "/sheffield-bros/static/js/2.6d68a20d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sheffield-bros/static/js/2.6d68a20d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8f248c301334960525",
+    "url": "/sheffield-bros/static/js/main.5539452c.chunk.js"
+  },
+  {
+    "revision": "e13efff120b119e0752d",
+    "url": "/sheffield-bros/static/js/runtime-main.74454282.js"
+  },
+  {
+    "revision": "3061b175706a2ffec82709f4baa724fd",
+    "url": "/sheffield-bros/static/media/alex.3061b175.jpg"
+  },
+  {
+    "revision": "4a7cc5dc43dbe90375413fa6da70f403",
+    "url": "/sheffield-bros/static/media/bobby.4a7cc5dc.jpg"
+  },
+  {
+    "revision": "1aac2eddd9bda5b24a025c178528434a",
+    "url": "/sheffield-bros/static/media/elliot.1aac2edd.jpg"
+  }
+]);
